10fc60bc02ffd2606d069fe18131f35e98fbdd1e
João Pedro Ficanha <jpfficanha@gmail.com>
2024-09-24 17:54:23 -0300

    first commit

---
+++ src/main/java/dev/itsjofi/simpledigitalwallet/SimpleDigitalWalletApplication.java
@@ -0,0 +1,13 @@
+package dev.itsjofi.simpledigitalwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDigitalWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDigitalWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/itsjofi/simpledigitalwallet/SimpleDigitalWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.itsjofi.simpledigitalwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDigitalWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
