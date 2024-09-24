92c8f58cce5704dba38ff8d65323f078a3f0a41e
João Pedro Ficanha <jpfficanha@gmail.com>
2024-09-24 18:15:30 -0300

    :sparkles: feat(controller): create wallet controller

---
+++ src/main/java/dev/itsjofi/simpledigitalwallet/controller/WalletController.java
@@ -0,0 +1,23 @@
+package dev.itsjofi.simpledigitalwallet.controller;
+
+import dev.itsjofi.simpledigitalwallet.controller.dto.CreateWalletDto;
+import dev.itsjofi.simpledigitalwallet.entity.Wallet;
+import dev.itsjofi.simpledigitalwallet.service.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WalletController {
+
+    @Autowired
+    private WalletService walletService;
+
+    @PostMapping("/wallet")
+    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDto createWalletDto) {
+        return new ResponseEntity<>(walletService.create(createWalletDto), HttpStatus.CREATED);
+    }
+}
