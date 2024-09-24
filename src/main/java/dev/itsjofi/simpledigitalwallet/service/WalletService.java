5e13fa07709d3cf996a6800607a21770d6ea4b2e
João Pedro Ficanha <jpfficanha@gmail.com>
2024-09-24 18:31:26 -0300

    :sparkles: feat(WalletService): encrypt password before saving to db

--- src/main/java/dev/itsjofi/simpledigitalwallet/service/WalletService.java
+++ src/main/java/dev/itsjofi/simpledigitalwallet/service/WalletService.java
@@ -3,6 +3,7 @@ package dev.itsjofi.simpledigitalwallet.service;
 import dev.itsjofi.simpledigitalwallet.controller.dto.CreateWalletDto;
 import dev.itsjofi.simpledigitalwallet.entity.Wallet;
 import dev.itsjofi.simpledigitalwallet.repository.WalletRepository;
+import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,12 @@ public class WalletService {
     @Autowired
     private WalletRepository walletRepository;
 
+    @Autowired
+    private StrongPasswordEncryptor passwordEncryptor;
+
     public Wallet create(CreateWalletDto createWalletDto) {
-        return walletRepository.save(createWalletDto.toWalletEntity());
+        Wallet wallet = createWalletDto.toWalletEntity();
+        wallet.setPassword(passwordEncryptor.encryptPassword(wallet.getPassword()));
+        return walletRepository.save(wallet);
     }
 }
