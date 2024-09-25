b9171ed419e98b553548c66cdd1ef344b0681953
João Pedro Ficanha <jpfficanha@gmail.com>
2024-09-25 10:43:55 -0300

    :sparkles: feat(SimpleDigitalWalletApplication): enable feign clients

--- src/main/java/dev/itsjofi/simpledigitalwallet/SimpleDigitalWalletApplication.java
+++ src/main/java/dev/itsjofi/simpledigitalwallet/SimpleDigitalWalletApplication.java
@@ -3,8 +3,10 @@ package dev.itsjofi.simpledigitalwallet;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEncryptableProperties
+@EnableFeignClients
 @SpringBootApplication
 public class SimpleDigitalWalletApplication {
 
