c21e10fdf475b8dfc4ef18add8828261bc8b973c
João Pedro Ficanha <jpfficanha@gmail.com>
2024-09-26 11:30:27 -0300

    :wrench: chore(SimpleDigitalWalletApplication.java): remove dotenv configuration

--- src/main/java/dev/itsjofi/simpledigitalwallet/SimpleDigitalWalletApplication.java
+++ src/main/java/dev/itsjofi/simpledigitalwallet/SimpleDigitalWalletApplication.java
@@ -1,7 +1,6 @@
 package dev.itsjofi.simpledigitalwallet;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,15 +11,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class SimpleDigitalWalletApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("JASYPT_ENCRYPTOR_PASSWORD", dotenv.get("JASYPT_ENCRYPTOR_PASSWORD"));
-
 		SpringApplication.run(SimpleDigitalWalletApplication.class, args);
 
 	}
